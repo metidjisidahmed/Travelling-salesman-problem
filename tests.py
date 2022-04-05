4c36a960fd6b3779c74679d6c8bff228175d0b17
Metidji Sid Ahmed <is_metidji@esi.dz>
2022-04-05 17:12:42 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,11 +6,12 @@ from timeit import timeit
 
 import pytest
 
+starting_node = "A"
 
 def testGreedy():
     time_before = datetime.datetime.now()
     graph = create_graph_from_excel_file("C:\\Users\\pc\\Documents\\GitHub\\TPGO_TP_4\\data.xlsx")
-    greedy_algorithm_init(graph, "A")
+    greedy_algorithm_init(graph, starting_node)
     time_after = datetime.datetime.now()
     print("Time Greedy algo =", (time_after - time_before).microseconds / 1000000, " Seconds ")
 
@@ -20,7 +21,7 @@ def testGreedy():
 def testComplete():
     time_before = datetime.datetime.now()
     graph = create_graph_from_excel_file("C:\\Users\\pc\\Documents\\GitHub\\TPGO_TP_4\\data.xlsx")
-    result = complete_algorithm_init(graph, "A")
+    result = complete_algorithm_init(graph, starting_node)
     time_after = datetime.datetime.now()
     print("Time Complete algo =", (time_after - time_before).microseconds / 1000000, " Seconds ")
     return True
